cb17200aea2e7c722f4609c7392ec7228a81cff2
chj_damon <chjdamon@gmail.com>
2020-01-06 22:55:44 +0800

    fix: 格式化代码

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -3,11 +3,7 @@ import React from 'react';
 export default (): React.ReactNode => (
   <p style={{ textAlign: 'center' }}>
     Want to add more pages? Please refer to{' '}
-    <a
-      href="https://pro.ant.design/docs/block-cn"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
+    <a href="https://pro.ant.design/docs/block-cn" target="_blank" rel="noopener noreferrer">
       use block
     </a>
     。
